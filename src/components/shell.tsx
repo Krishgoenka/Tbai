9d717054837ba2327d1b832f9ee48cad6590e368
Krish Goenka <goenkakrish02@gmail.com>
2025-08-22 14:02:03 +0000

    still same .. check the fyll code 2 times

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -1,11 +1,20 @@
 "use client"
 
+import { useAuth } from "@/hooks/use-auth"
 import { SidebarProvider } from "@/components/ui/sidebar"
 
 export function AppShell({ children }: { children: React.ReactNode }) {
-  return (
-    <SidebarProvider>
-      {children}
-    </SidebarProvider>
-  )
+  const { user } = useAuth()
+
+  if (user) {
+    return (
+      <SidebarProvider>
+        <div className="flex">
+          {children}
+        </div>
+      </SidebarProvider>
+    )
+  }
+
+  return <>{children}</>
 }
