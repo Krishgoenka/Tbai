01ffb95869be03cd37e9fcdb304f01e87a80eb06
Krish Goenka <goenkakrish02@gmail.com>
2025-08-22 14:18:52 +0000

    same problem unauthorised domain

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -8,14 +8,14 @@ import { getStorage } from "firebase/storage";
 const firebaseConfig = {
   projectId: "technobillion-ai-platform",
   appId: "1:543559355076:web:b3f7e5a07c3dcb6bfad791",
-  storageBucket: "technobillion-ai-platform.firebasestorage.app",
+  storageBucket: "technobillion-ai-platform.appspot.com",
   apiKey: "AIzaSyBrT_oYIYok1KwSmzAeZir73hCVafilbTg",
   authDomain: "technobillion-ai-platform.firebaseapp.com",
   messagingSenderId: "543559355076",
 };
 
-// Initialize Firebase
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase for SSR
+const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 const storage = getStorage(app);
