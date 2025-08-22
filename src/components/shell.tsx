d7b3f46c0695b541a91f20e77a94fa1eb5f72e13
Krish Goenka <goenkakrish02@gmail.com>
2025-08-22 14:01:19 +0000

    the whole website is on left . something is worng

--- src/components/shell.tsx
+++ src/components/shell.tsx
@@ -5,9 +5,7 @@ import { SidebarProvider } from "@/components/ui/sidebar"
 export function AppShell({ children }: { children: React.ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="flex">
-        {children}
-      </div>
+      {children}
     </SidebarProvider>
   )
 }
