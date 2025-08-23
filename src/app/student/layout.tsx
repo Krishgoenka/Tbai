f4a00a17722569d5f607cbcea866130d1dbe1d18
Krish Goenka <goenkakrish02@gmail.com>
2025-08-23 17:57:05 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/student/layout.tsx
+++ src/app/student/layout.tsx
@@ -1,5 +1,6 @@
 
 import { DashboardHeader } from "@/components/dashboard/header";
+import { SidebarProvider } from "@/components/ui/sidebar";
 
 export default function StudentLayout({
   children,
@@ -7,9 +8,11 @@ export default function StudentLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex-1 flex flex-col">
-        <DashboardHeader />
-        <main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main>
-    </div>
+    <SidebarProvider>
+        <div className="flex-1 flex flex-col">
+            <DashboardHeader />
+            <main className="flex-1 p-4 md:p-6 lg:p-8">{children}</main>
+        </div>
+    </SidebarProvider>
   );
 }
